3db87f7ef5dfac5d0cba07ccb50fe97cfd14bfc4
Dave Holst <46893433+daveholst@users.noreply.github.com>
2022-03-03 07:53:23 +0800

    biasing circle for search

--- src/Components/Map.svelte
+++ src/Components/Map.svelte
@@ -16,6 +16,7 @@
     let friendsArray = [];
 
     let centerPoint;
+    let searchRadius = 500; //is this m? ft miles?
     let service;
 
     let friendsPolygon = new google.maps.Polygon({
@@ -26,6 +27,17 @@
         fillColor: "#FF0000",
         fillOpacity: 0.35,
     });
+
+    let searchCircle = new google.maps.Circle({
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        center: wan,
+        radius: 500,
+    });
+
     function getCenter(points) {
         let sumX = 0;
         let sumY = 0;
@@ -38,22 +50,53 @@
         return { lat: sumX / points.length, lng: sumY / points.length };
     }
 
-    function drawPolygon(pointsArray) {
-        if (pointsArray.length > 1) {
-            // wipe existing polygon
-            friendsPolygon.setMap(null);
-            // build polygon
-            friendsPolygon = new google.maps.Polygon({
-                paths: friendsArray,
-                strokeColor: "#FF0000",
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: "#FF0000",
-                fillOpacity: 0.35,
-            });
+    function haversineDistance(mk1, mk2) {
+        const R = 6371; // Radius of the Earth in km
+        const rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
+        const rlat2 = mk2.lat * (Math.PI / 180); // Convert degrees to radians
+        const difflat = rlat2 - rlat1; // Radian difference (latitudes)
+        const difflon = (mk2.lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+
+        const d =
+            2 *
+            R *
+            Math.asin(
+                Math.sqrt(
+                    Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+                        Math.cos(rlat1) *
+                            Math.cos(rlat2) *
+                            Math.sin(difflon / 2) *
+                            Math.sin(difflon / 2)
+                )
+            );
+        return d * 1000; // Radius in km
+    }
+
+    function getRadius(center, coordArray) {
+        let maxDistance = 0;
+        coordArray.forEach((point) => {
+            let calculatedDistance = haversineDistance(center, point);
+            if (calculatedDistance > maxDistance) {
+                maxDistance = calculatedDistance;
+            }
+        });
+        return maxDistance * 0.75; // magic number to keep circle smaller
+    }
+
+    function drawCircle(coordArray) {
+        if (coordArray.length > 1) {
+            // calc center
+            const searchCenter = getCenter(coordArray);
+            // change center
+            searchCircle.setCenter(searchCenter);
+            // calc Radius
+            const searchRadius = getRadius(searchCenter, coordArray);
+            console.log("SR::", searchRadius);
+            // change radius
+            searchCircle.setRadius(searchRadius);
+
             // apply to map
-            friendsPolygon.setMap(map);
-            console.log("getCenter:: ", getCenter(friendsArray));
+            searchCircle.setMap(map);
         }
     }
 
@@ -96,30 +139,19 @@
                 radius: "500", //TODO do a proper calc here
                 type: ["cafe"],
             };
-
-            console.log(event.latLng.lat());
             addMarker(event.latLng, map);
-            drawPolygon(friendsArray);
-            console.log("cent:: ", getCenter(friendsArray));
-            cafeRequest.location = await getCenter(friendsArray);
-            console.log("centerP:: ", centerPoint);
-            // dra  wCenter(getCenter(friendsArray));
-            console.log("cafe req::", cafeRequest);
+            const calculatedCenter = await getCenter(friendsArray);
+            cafeRequest.location = calculatedCenter;
+            drawCircle(friendsArray);
+
             service.nearbySearch(cafeRequest, function (results, status) {
                 console.log(status);
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
                     console.log(results);
                     // add to store
                     SearchResultsStore.update(() => results);
-
-                    //  for (var i = 0; i < results.length; i++) {
-                    // createMarker(results[i]);
                 }
-                //   map.setCenter(results[0].geometry.location);
-                // }
             });
-
-            // });
         });
     });
 </script>
